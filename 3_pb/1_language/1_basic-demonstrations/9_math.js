dd9ff20ce7f3f3265446dc76a606a22feba36d4d
FRickReich <frickreich@gmail.com>
2024-04-16 11:38:41 +0200

    Add advanced math examples

---
+++ 3_pb/1_language/1_basic-demonstrations/9_math.js
@@ -0,0 +1,26 @@
+console.log('='.repeat(25) + 'DIE MATH BIBLIOTHEK' + '='.repeat(25));
+
+// Um mathematische berechnungen und methoden zu vereinfachen, bietet uns javascript die Math bibliothek, die verschieden methoden beinhaltet, mit der wir zahlen bearbeiten können.
+
+// link zur MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+console.log('='.repeat(25) + 'Auf und abrunden' + '='.repeat(25));
+
+console.log('Math.ceil();');
+// mit der methode .ceil(); können wir eine kommazahl aufrunden.
+console.log('Aufgerundet', Math.ceil(2.3));
+
+console.log('Math.floor();');
+// mit der methode .floor(); können wir eine kommazahl abrunden.
+console.log('Abgerundet', Math.floor(2.7));
+
+console.log('Math.round();');
+// zur nächstgelegenen zahl runden könnnen wir mit der methode .round();
+console.log('Runden', Math.round(2.2));
+console.log('Runden', Math.round(2.7));
+
+console.log('='.repeat(25) + 'PI' + '='.repeat(25));
+
+console.log('Math.PI');
+// um pi zu bekommen, können wir die property .PI benutzen.
+console.log('Pi:', Math.PI);
