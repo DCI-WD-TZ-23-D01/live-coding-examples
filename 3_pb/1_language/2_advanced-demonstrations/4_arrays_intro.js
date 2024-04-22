8a5eaca0fdb45f89cff8bfdd66ba84d3913bd418
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-04-22 10:52:04 +0200

    added array day

---
+++ 3_pb/1_language/2_advanced-demonstrations/4_arrays_intro.js
@@ -0,0 +1,72 @@
+/* ----------------------------------------
+
+22.04.2024 | Arrays - Intro + Methoden
+
+- Was sind Arrays?
+- Array-Grundlagen (Index, Length)
+- Array-Methoden
+
+---------------------------------------- */
+
+// Stellt euch überall einfache Anführungzeichen ('') vor, ich persönlich schreibe weiterhin doppelte ("")
+
+// ---------- ARRAY GRUNDLAGEN ----------
+
+// Bisherige "primitive" Datentypen: Strings (Zeichenketten), Numbers (Zahlen), Booleans (Wahrheitswerte) - d.h. jede Variable hat nur einen Wert
+
+const string = "Hallo Klasse!"; 
+let anzahl = 3;
+let binIchEingeloggt = true;
+
+// Neu: "fortgeschrittener" (advanced) Datentyp - eine Variable kann mehrere Werte enthalten
+const emptyNames = []; // Datenty Array
+console.log(emptyNames);
+
+// Ein Array ist eine Liste von vielen Dingen
+// Variablenname wird meist nach Inhalt und in Mehrzahl-Form angelegt
+// Liste mit Namen -> names, Liste mit Produkten -> products
+
+const names = ["Hans","Petra","Isabel"];
+console.log(names);
+
+console.log(names[0]); // Ausgabe 1. Element des Arrays
+
+// ---------- ZUGRIFF AUF ELEMENTE IM ARRAY ----------
+
+// Man kann unterschiedliche Datentypen in ein Array stecken
+
+// Element      1.      2.   3.  4.    5.
+// Pos/Index    0       1    2   3     4
+
+const years = [1882, "2058", 3, 105, true];
+console.log(years);
+
+console.log(years.length);
+
+console.log(years[years.length -1]); // Letzes Element im Array
+
+// ---------- ZUGRIFF/ÜBERSCHREIBEN VON ELEMENTEN IM ARRAY ----------
+
+// Arrays immer mit const deklarieren, ansonsten kann man es überschreiben, bzw. den Datentypen ändern
+
+// let products = ["Staubsauger","Gummistiefel","Quietschente","Eis"];
+// products = "Huch, Array ist weg?";
+
+const products = ["Staubsauger","Gummistiefel","Quietschente","Eis"];
+
+// Zur Erinnerung: Variablen konnten nur mit Variablennamen einen neuen Wert bekommen (Zuweisung)
+// let zahl = 4;
+// zahl = 3;
+
+// Hier: Zuweisung für neuen Wert *eines Elements* im Array
+// Hinzufügen neues Element am Index 4 (Array ging vorher nur bis Index 3)
+products[4] = "Kaffeekanne";
+// kein Element am Index 5
+products[6] = "Butter";
+
+console.log(products);
+console.log(products.length); // Wenn man ein Element auslässt, wird dieses in .length mitgezählt
+
+products[0] = "Staub"; // Eigentlich Staubsauger, überschreibt Element
+console.log(products);
+
---
+++ 3_pb/1_language/2_advanced-demonstrations/5_arrays_methoden.js
@@ -0,0 +1,87 @@
+// Array-Methoden: Hinzufügen / Entfernen von Elementen
+// Teil-Arrays, Suchen im Array, String zu Array, Array zu String
+
+console.log("--------- PUSH/POP ---------");
+
+const products = ["Staubsauger","Gummistiefel","Quietschente","Eis"];
+console.log("Array vorher:",products);
+
+// PUSH: neues Element ans Ende des Arrays hinzufügen
+products.push("Mikrofon");
+console.log("Array nach push():",products);
+
+// POP: letztes Element im Array löschen
+products.pop();
+console.log("Array nach pop():",products);
+
+console.log("--------- SHIFT/UNSHIFT ---------");
+
+// UNSHIFT: neues Element wird am Anfang des Arrays hinzugefügt
+console.log("Array vorher:",products);
+
+products.unshift("Decke");
+console.log("Array nach unshift():",products);
+
+// SHIFT: erstes Element im Array löschen
+products.shift();
+console.log("Array nach shift():",products);
+
+// Mehrmals nacheinander löschen
+products.shift();
+products.shift();
+console.log("Array nach 2x mehr shift():",products);
+
+// -------- SLICE: Teile des Arrays --------
+
+const animals = ["Katze","Hund","Elefant","Zebra"];
+console.log("Alle Tiere:", animals);
+
+const pets = animals.slice(0,2);
+console.log("Teil-Array nur mit Haustieren:", pets);
+console.log(animals);
+
+// negative Werte für Index zählen vom Ende des Arrays
+// Wenn wir 2. Wert in slice() weglassen, dann wird der ganze Rest des Arrays genommen
+const zooAnimals = animals.slice(-2);
+console.log(zooAnimals);
+
+// -------- INCLUDES: Suchen nach Vorkommen im Array --------
+
+console.log("Array animals includes Katze?", animals.includes("Katze"));
+console.log("Array animals includes Katze?", animals.includes("katze"));
+
+// -------- indexOf: Suchen nach Position im Array --------
+
+// Falls Element im Array: Rückgabewert: Index des Elemements
+console.log("Wo ist Hund im Array?", animals.indexOf("Hund"));
+// Falls Element nicht im Array: Rückgabewert: -1
+console.log("Wo ist hund im Array?", animals.indexOf("hund"));
+
+// -------- Umwandlung String -> Array --------
+
+const string = "Hallo Klasse!";
+console.log(string);
+
+// Zeichen IM split wird als eine Art Trennungssymbol benutzt und landet nicht im Array, hier: Trennungssymbol: Leerzeichen -> Ergebnis Worte
+const words = string.split(" ");
+console.log(words);
+
+// Trennungszeichen: "nichts" -> Ergebnis alle Buchstaben
+const letters = string.split("");
+console.log(letters);
+
+// Ergibt nicht viel Sinn in diesem Beispeil, wird aber manchmal benutzt
+const keineLs = string.split("l");
+console.log(keineLs);
+
+// Nachdem der String in ein Array umgewandelt wurde, können wir Elemente ändern, z.B. das erste (Index 0) Wort ersetzen
+words[0] = "Tschüss";
+console.log(words);
+
+// Ohne Angabe/Wert in join() wird automatisch "," als Verbindungszeichen benutzt
+const newString = words.join(); 
+console.log(newString);
+
+// Mit Angabe/Wert im join, wird diese benutzt um die Elemente des Arrays zusammen zu fügen
+const greeting = words.join(" "); 
+console.log(greeting);
