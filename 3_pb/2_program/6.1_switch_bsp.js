529537b4ddc92ebd1a5949262dd41a9656d3aa58
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-04-29 10:25:12 +0200

    mehr beispiele für blockscope in switch

--- 3_pb/2_program/6.1_switch_bsp.js
+++ 3_pb/2_program/6.1_switch_bsp.js
@@ -4,12 +4,59 @@ const bahncard = 100;
 const kosten = 50;
 
 switch (bahncard) {
-    case 25:
+    case 25: {
         console.log("Preis Fahrkarte:", kosten * 0.75);
         break;
-    case 50:
+        }
+    case 50: {
         console.log("Preis Fahrkarte:", kosten * 0.5);
         break;
+        }
+    case 100:
+        console.log("Keine weiteren Kosten. Preis: 0 EUR.");
+        break;
+    default:
+        console.log("Keine gültige Bahncard! Preis:", kosten);
+}
+
+// --------------------------------------------------
+
+// Variablen anlegen innerhalb der unterschiedlichen Fälle erfordert einen neuen Block
+
+switch (bahncard) {
+    case 25: {
+        const rabatt = 0.75;
+        console.log("Preis Fahrkarte:", kosten * rabatt);
+        break;
+        }
+    case 50: {
+        const rabatt = 0.5;
+        console.log("Preis Fahrkarte:", kosten * rabatt);
+        break;
+        }
+    case 100:
+        console.log("Keine weiteren Kosten. Preis: 0 EUR.");
+        break;
+    default:
+        console.log("Keine gültige Bahncard! Preis:", kosten);
+}
+
+// --------------------------------------------------
+// Variable außerhalb anlegen und innerhalb der Fälle nur Wert ändern
+
+let rabatt = 0;
+
+switch (bahncard) {
+    case 25: {
+        rabatt = 0.75;
+        console.log("Preis Fahrkarte:", kosten * rabatt);
+        break;
+        }
+    case 50: {
+        rabatt = 0.5;
+        console.log("Preis Fahrkarte:", kosten * rabatt);
+        break;
+        }
     case 100:
         console.log("Keine weiteren Kosten. Preis: 0 EUR.");
         break;
