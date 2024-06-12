499c5f9c5f1bfe7e85ec5f8e5798c67666ce89c3
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-06-12 12:30:41 +0200

    last update

--- 3_pb/5_functions2/2_adv-array-methods/2_filter.js
+++ 3_pb/5_functions2/2_adv-array-methods/2_filter.js
@@ -34,10 +34,24 @@ const products = [
 ];
 
 // Ich möchte nur die schwarzen (black) Produkte haben
-console.clear();
 const onlyBlackProducts = products.filter(
   (product) => product.color === "black"
 );
 console.log(onlyBlackProducts);
 
 // Aufgabe: Ich möchte nur die Produkte haben, die zwischen 100 und 200 € kosten (100 und 200 inklusiv) (Smartwatch, Keyboard, Printer)
+const between100and200 = products.filter(
+  (product) => product.price >= 100 && product.price <= 200
+);
+console.clear();
+console.log(between100and200);
+const onlyName = between100and200.map((product) => product.name);
+console.log(onlyName);
+
+// Dadurch dass filter (und map) ein Array zurückgibt, kann man die Methode hintereinander schreiben
+const hintereinander = products
+  .filter((product) => product.price >= 100 && product.price <= 200)
+  .map((product) => product.name);
+// .push('a') // push returnt die Länge des neuen Arrays
+// .map(() => {}); // <- deswegen geht das nicht
+console.log(hintereinander);
