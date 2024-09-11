79eb3245917524c47d38a2ef2542c5f4b51814d3
FRickReich <frickreich@gmail.com>
2024-09-11 10:25:51 +0200

    Add Page component

--- 4_spa/3_react/4_props/src/App.jsx
+++ 4_spa/3_react/4_props/src/App.jsx
@@ -2,21 +2,52 @@ import reactLogo from './assets/react.svg'; // <=== so machen wir das, mit dem d
 import viteLogo from '/vite.svg'; // <=== das ist nicht der beste weg
 
 import { 
-  Header
+  Header,
+  Page
 } from './components';
 
 function App() {
+
+  const students = [
+    'Chrissi',
+    'Claire',
+    'Danny',
+    'Ines',
+    'Jeery',
+    'Lara',
+    'Marie',
+    'Mehmet',
+    'Minas',
+    'Ming',
+    'Naheeda',
+    'Natalia',
+    'Nell',
+    'Sab',
+    'Sabine',
+    'Sebastian',
+    'Simia',
+    'Wael',
+    'Xiangyu',
+    'Zahra',
+    'Zedan',
+  ];
 
   return (
     <>
       {/* um props zu übergeben, schreiben wir sie als argumente in unser JSX element */}
       {/* equivalent zu Header({ title: "Hallo Welt" }); */}
-      <Header title="Hallo Welt" subTitle="Ich bin der Subtitle!"/>
-      <Header title="Hallo Claire"/>
 
       {/* bei mehreren props ist es übersichtlicher, sie untereiannder zu schreiben */}
       <Header 
-        title="Hallo Christian" 
+        title="Hallo Welt" 
+        subTitle="Ich bin der Subtitle!"
+      />
+
+      {/* wir setzen unseren Page komponenten ein */}
+      <Page
+        title="Unterseite"
+        bild={reactLogo}
+        studentsList={students}
       />
     </>
   );
--- 4_spa/3_react/4_props/src/components/index.js
+++ 4_spa/3_react/4_props/src/components/index.js
@@ -1,5 +1,7 @@
 import { Header } from "./Header/Header";
+import { Page } from "./Page/Page";
 
 export { 
-    Header
+    Header,
+    Page
 }
